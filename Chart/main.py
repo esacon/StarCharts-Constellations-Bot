cd6a2b94293100bf3ac635fb00f00711aa04be2f
Esaco <esaco@uninorte.edu.co>
2020-05-18 20:59:27 -0500

    Comentarios añadidos, requiriments.txt actualizado

--- Chart/main.py
+++ Chart/main.py
@@ -2,12 +2,12 @@
 import numpy as np
 import cv2
 
-
 stars = []
 names = ['boyero', 'casiopea', 'cazo', 'cygnet', 'geminis', 'hydra', 'osamayor', 'osamenor']
-img = np.zeros((1000, 1000, 3), dtype=np.uint8)
+img = np.zeros((1000, 1000, 3), dtype=np.uint8)  # Establece una imagen como una matriz
 
 
+# Recorre el archivo 'stars.txt' y separa los datos del mismo para almacenarlo en una lista
 def readStars():
     file = open('Datos/stars.txt', 'r')
     i = 0
@@ -24,6 +24,7 @@ def readStars():
     file.close()
 
 
+# Recibe un archivo de constelaciones y separa sus datos en un arreglo
 def readConstellation(file):
     const = []
     i = 0
@@ -36,6 +37,7 @@ def readConstellation(file):
     return const
 
 
+# Recibe un parámetro nombre y selecciona el archivo correspondiente
 def find_constellation(name):
     files = [open('Datos/Constelaciones/Boyero.txt', 'r'), open('Datos/Constelaciones/Casiopea.txt', 'r'),
              open('Datos/Constelaciones/Cazo.txt', 'r'), open('Datos/Constelaciones/Cygnet.txt', 'r'),
@@ -48,10 +50,12 @@ def find_constellation(name):
         return -1
 
 
+# Convierte las coordenadas cartesianas a pixeles para manipular la imagen de mejor manera
 def cart_to_pixel(xCoord, yCoord):
     return int(500*(xCoord + 1)), int(500*(1 - yCoord))
 
 
+# Dibuja las estrellas en una imagen vacía de fondo negro
 def drawStars():
     for star in stars:
         star_size = round(10 / (float(star[4]) + 2))
@@ -64,6 +68,7 @@ def drawStars():
     cv2.imwrite("Images/stars.png", img)
 
 
+# Se encarga de dibujar una constelación que recibe como parámetro
 def drawConstellations(const, color, img2):
     i = 0
     while i < len(const[0]):
@@ -71,7 +76,9 @@ def drawConstellations(const, color, img2):
         pos = []
         while j < 2:
             for star in stars:
+                # Valida que la estrella esociga cuente con el parámetro nombre
                 if len(star) > 6:
+                    # Compara el nombre dentro de cada constelación con cada nombre posible para una estrella
                     if len(star[6]) == 1:
                         if const[0][i][j] == star[6][0]:
                             pos.append(star[0])
@@ -88,12 +95,14 @@ def drawConstellations(const, color, img2):
         x, y = cart_to_pixel(float(pos[0]), float(pos[1]))
         x1, y1 = cart_to_pixel(float(pos[2]), float(pos[3]))
         cv2.line(img2, (x, y), (x1, y1), color, 1)
+        # En caso que varias estrellas tengan un mismo nombre, se grafica también la conexión entre ambos puntos
         if len(pos) > 4:
             x2, y2 = cart_to_pixel(float(pos[4]), float(pos[5]))
             cv2.line(img2, (x1, y1), (x2, y2), color, 1)
         i += 1
 
 
+# Grafica cada constelación de un color distinto
 def drawAllConst():
     color = [(0, 255, 255), (255, 0, 255), (255, 255, 0), (0, 255, 0), (0, 0, 255), (34, 126, 230), (7, 182, 23),
              (254, 19, 188)]
@@ -107,6 +116,7 @@ def drawAllConst():
     cv2.imwrite("Images/space.png", img)
 
 
+# Genera un archivo .png para cada constelación
 def generateConst():
     for name in names:
         img2 = np.copy(img)
--- main_bot.py
+++ main_bot.py
@@ -10,19 +10,20 @@ def main():
     updater = Updater(TOKEN, use_context=True)  # Insertar Token del bot
     dispatcher = updater.dispatcher
 
+    # Controla el menú de opciones
     dispatcher.add_handler(CommandHandler('start', bot.start))
     dispatcher.add_handler(CommandHandler('menu', bot.menu))
     dispatcher.add_handler(CallbackQueryHandler(bot.menubutton, pattern="m1"))
     dispatcher.add_handler(CallbackQueryHandler(bot.menubutton, pattern="m2"))
     dispatcher.add_handler(CallbackQueryHandler(bot.menubutton, pattern="m3"))
-
+    # Maneja la ayuda que provee programa para su funcionamiento
     dispatcher.add_handler(CommandHandler('help', bot.help))
-
+    # Selecciona la imagen correspondiente
     dispatcher.add_handler(CommandHandler("constelaciones", bot.constelaciones))
     dispatcher.add_handler(CallbackQueryHandler(bot.button))
     dispatcher.add_handler(CommandHandler("stars", bot.stars))
     dispatcher.add_handler(CommandHandler("space", bot.space))
-
+    # Lleva el control de los errores ocasionados en el programa
     dispatcher.add_error_handler(bot.error)
 
     updater.start_polling()
